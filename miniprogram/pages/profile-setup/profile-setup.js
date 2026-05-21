404eeb88fa260452aa97f18386a9155167bf7834
Gzy-HaiTang <1402375281@qq.com>
2026-05-21 14:42:33 +0800

    simplify: profile-setup 只获取昵称，去掉头像选择

--- miniprogram/pages/profile-setup/profile-setup.js
+++ miniprogram/pages/profile-setup/profile-setup.js
@@ -2,12 +2,7 @@ const { request } = require('../../utils/request')
 
 Page({
   data: {
-    avatarUrl: '',
     nickname: ''
-  },
-
-  onChooseAvatar(e) {
-    this.setData({ avatarUrl: e.detail.avatarUrl })
   },
 
   onNicknameInput(e) {
@@ -15,47 +10,21 @@ Page({
   },
 
   saveProfile() {
-    const { avatarUrl, nickname } = this.data
+    const { nickname } = this.data
     if (!nickname.trim()) {
       wx.showToast({ title: '请输入昵称', icon: 'none' })
       return
     }
 
     wx.showLoading({ title: '保存中...' })
-
-    // 如果有头像，先上传再保存
-    if (avatarUrl) {
-      wx.uploadFile({
-        url: require('../../utils/config').API_BASE_URL + '/api/v1/user/avatar',
-        filePath: avatarUrl,
-        name: 'file',
-        header: {
-          Authorization: 'Bearer ' + wx.getStorageSync('token')
-        },
-        success: (res) => {
-          const data = JSON.parse(res.data)
-          this.saveToBackend(nickname, data.avatar_url)
-        },
-        fail: () => {
-          // 上传失败，只保存昵称
-          this.saveToBackend(nickname, '')
-        }
-      })
-    } else {
-      this.saveToBackend(nickname, '')
-    }
-  },
-
-  saveToBackend(nickname, avatarUrl) {
     request({
       url: '/api/v1/user/profile',
       method: 'POST',
-      data: { nickname, avatar_url: avatarUrl }
+      data: { nickname }
     }).then(() => {
       const app = getApp()
       if (app && app.globalData.userInfo) {
         app.globalData.userInfo.nickname = nickname
-        if (avatarUrl) app.globalData.userInfo.avatar_url = avatarUrl
       }
       wx.hideLoading()
       wx.showToast({ title: '设置成功', icon: 'success' })
